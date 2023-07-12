2277a845f89669158ce54a2d6c2bf3d18410489d
BEDROCK\abhishek.swaminathan <abhishek.swaminathan@softwaresolved.com>
2023-07-12 16:12:49 +0100

    UnitProject with No Implementation

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public interface IFizzBuzz
+{
+    string GetOutput(int number);
+}
+
+public class FizzBuzz : IFizzBuzz
+{
+    public string GetOutput(int number)
+    {
+        throw new NotImplementedException();
+    }
+}
+
---
+++ FizzBuzzUnitTestProject/FizzBuzzTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class FizzBuzzTests
+{
+    private IFizzBuzz fizzBuzz;
+
+    [SetUp]
+    public void SetUp()
+    {
+        fizzBuzz = new FizzBuzz();
+    }
+
+    [Test]
+    public void GetOutput_DivideBy3_ReturnsFizz()
+    {
+        var output = fizzBuzz.GetOutput(3);
+        Assert.AreEqual("Fizz", output);
+    }
+
+    [Test]
+    public void GetOutput_DivideBy5_ReturnsBuzz()
+    {
+        var output = fizzBuzz.GetOutput(5);
+        Assert.AreEqual("Buzz", output);
+    }
+
+    [Test]
+    public void GetOutput_DivideBy3And5_ReturnsFizzBuzz()
+    {
+        var output = fizzBuzz.GetOutput(15);
+        Assert.AreEqual("FizzBuzz", output);
+    }
+
+    [Test]
+    public void GetOutput_NotDivideBy3Or5_ReturnsNumber()
+    {
+        var output = fizzBuzz.GetOutput(7);
+        Assert.AreEqual("7", output);
+    }
+}
